388e706e414a5fd253a60099de0f703af133587f
Aman Ankit <141806230+AmanAnkit06@users.noreply.github.com>
2026-02-19 21:08:27 +0530

    Time: 2 ms (73.19%) | Memory: 15.2 MB (9.92%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,13 +3,24 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
 
         // apply brute force--------
-        for(int i=0;i<nums.size();i++){
+      /*  for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]+nums[j]==target){
                     return {i,j};
                 }
             }
+        }*/
+
+        //apply optimal using map ds----
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            auto it=mp.find(target-nums[i]);
+            if(it!=mp.end()){
+                return {mp[target-nums[i]],i};
+            }
+             mp[nums[i]]=i;
         }
+        
         return {};
     }
 };
